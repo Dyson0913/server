c2788f8aff4f824c17e35c979a263245f7110245
dyson0913 <hhg4092@gmail.com>
2016-03-04 16:42:35 +0000

    db add

---
+++ db/noSQL/myredis.py
@@ -0,0 +1,21 @@
+import redis
+#print dir(redis)
+# "redis": {"host":"localhost","port":6379, "db":0},
+
+
+# conn_info = self._redis_conn.get(msg[KEY_GAME_ID])
+#self._redis_conn.set(msg[KEY_GAME_ID], json.dumps(conn_info))
+def main():
+
+    redis_server = redis.StrictRedis(host='localhost', port=6379,password='dyson')
+#    redis_server.flushall()
+
+    redis_server.set("redis_test","maintest")
+    if redis_server.get("redis_test") == "maintest":
+         print "ok"
+    else:
+         print "not ok"
+
+if __name__ == "__main__":
+
+    main()
