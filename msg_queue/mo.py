a3a6362e4445afb7d5c63e558d0dbacb843b57f3
dyson0913 <hhg4092@gmail.com>
2016-03-06 09:15:10 +0000

    dir sort

--- msg_queue/mo.py
+++ msg_queue/mo.py
@@ -1,9 +1,12 @@
 import sys,os
 
-#sys.path.append(os.path.join(os.path.dirname(__file__), '..','plugin'))
-sys.path.append('plugins')
 
-import plugins
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__),'..', '..','db','plugins'))
+sys.path.append('../../db')
+
+from plugins import *
+
 from importlib import import_module 
 
 
@@ -12,15 +15,23 @@ class load(object):
         self.plugins = []
         self.myplugins = []
         self.config = ['myplugin1','myplugin2']
+ 
+        print sys.path[0]
+        print os.path.realpath(os.path.dirname(sys.path[0]))
+        self.mypath = os.path.realpath(os.path.dirname(sys.path[0]))
       
     def dynamicLoadModules(self):
-        print sys.path
-        sys.modules['plugins'] = self.plugins = type(sys)('plugins')
-        self.plugins.__path__ = []
-        for path in self.config:
-            mypath = os.path.join(sys.path[0],'plugins',path)
-            print mypath
-            self.plugins.__path__.append(mypath)
+        print sys
+        print sys.modules['plugins']
+        #
+#        sys.modules['plugins'] = self.plugins = type(sys)('just_init')
+#        print sys.modules['plugins']
+#        self.plugins.__path__ = []
+#        for path in self.config:
+#            mypath = os.path.join(self.mypath,'plugins',path)
+#            mypath = os.path.join(sys.path[0],'plugins',path)
+#            print mypath
+#            self.plugins.__path__.append(mypath)
         ##dynamic load modules
         self.modules = []
         self.modules = [ import_module(module) for module in self.config]
