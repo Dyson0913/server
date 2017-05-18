fb566e2b7f82ce1548dec68f7f3b213d4ae60296
dyson0913 <hhg4092@gmail.com>
2017-05-18 15:14:08 +0000

    credit asking

--- modules/credit/creditHandler.py
+++ modules/credit/creditHandler.py
@@ -4,11 +4,11 @@ _db = None
 
 def handle(json_msg,socket_list):
 
-    rep = handle(json_msg,socket_list)
+    rep = normal_handle(json_msg,socket_list)
     socket = socket_list[0]
     socket.send_json(rep)
 
-def handle(json_msg,socket_list):
+def normal_handle(json_msg,socket_list):
 
     global _db
     _db = socket_list[1]
@@ -19,25 +19,23 @@ def handle(json_msg,socket_list):
        rep = header(json_msg)
        rep['state'] = "userCredit_update"
        rep['total_Credit'] = key_check(json_msg['uuid'])
-
        return rep
 
 
 def header(json_msg):
     rep = dict()
     rep['uuid'] = json_msg['uuid']
+    rep['key'] = json_msg['uuid']
+    rep['for_db'] = None
     return rep
 
 def key_check(id):
     #get acc from db
     global _db
     acc = _db.get(id)
-    print acc
     if acc != None:
         playerstate = get_info(acc)
-        print playerstate
-        credit_info = get_info(playerstate['for_db']['playerinfo']['credit'])
-        print credit_info
+        credit_info = playerstate['for_db']['playerinfo']['credit']
         return credit_info['total']
     else:
        # illegle acc ,return 0 point
