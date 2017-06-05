4d20fc8185fd367a27cf23ee942a5283a51f2115
Dyson0913 <hhg4092@gmail.com>
2017-06-06 00:08:34 +0800

    bet test

--- test/unit/modules/bet/betHandler_test.py
+++ test/unit/modules/bet/betHandler_test.py
@@ -14,8 +14,7 @@ class betTestCase(unittest.TestCase):
 
     # prepare work before every test
     def setUp(self):
-        self.info = {'cmd': "bet", "uuid":"111"}
-        self.bet = {'cmd': "bet", "game_id":"ba_1", 'bet':[{'type':1,'amount':100},{'type':2,'amount':200}]}
+        self.bet = {'cmd': "bet", "uuid":"111","game_id":"ba_1", 'bet':[{'type':1,'amount':100},{'type':2,'amount':200}]}
 
     # clean work after every test
     def tearDown(self):
@@ -28,7 +27,7 @@ class betTestCase(unittest.TestCase):
         mock_db_socket = Mock()
         mock_db_socket.get.return_value = json.dumps({'state':"self_close",'for_db':{'playerinfo':{"credit": {"total":10000,"ba_1":2000}}}})
         mock_push_socket = Mock()
-        mock_rsp = normal_handle(self.info, [mock_push_socket, mock_db_socket])
+        mock_rsp = normal_handle(self.bet, [mock_push_socket, mock_db_socket])
      #   self.assertDictContainsSubset(rsp, mock_rsp)
 
     #    self.assertEqual(len(self.fsm._all_state), 2)
