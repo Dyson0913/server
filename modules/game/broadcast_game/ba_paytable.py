a2212659c9fe0de3b605ab36aa634bac06d47251
Dyson0913 <hhg4092@gmail.com>
2017-06-14 14:54:49 +0800

    paytable refacot

--- modules/game/broadcast_game/ba_paytable.py
+++ modules/game/broadcast_game/ba_paytable.py
@@ -9,18 +9,18 @@ Baccarat_banker_pair = 3
 Baccarat_player_pair = 4
 
 #odds
-ba_odds_11 = 1
-ba_odds_8 = 2
-ba_odds_095 = 3
-ba_odds_1 = 4
-ba_odds_0 = 5
+ba_odds_0 = 0
+ba_odds_095 = 1
+ba_odds_1 = 2
+ba_odds_8 = 3
+ba_odds_11 = 4
 
 ba_odds = {
-           "1":{"odds":12},
-           "2":{"odds":9},
-           "3":{"odds":1.95},
-           "4":{"odds":2},
-           "5":{"odds":0},
+            "0": {"odds": 0},
+            "1":{"odds":1.95},
+            "2": {"odds": 2},
+            "3":{"odds":12},
+            "4":{"odds":9},
           }
 
 def split_symbol():
@@ -33,17 +33,16 @@ def paytable(winstate):
     winzone = winstate.split(zone_split)[:-1]
 
     for winlist in winzone:
-        info = winlist.split(odd_split)
-    	if str(Baccarat_tie) == info[0]:
-            paytable = [1] * (Baccarat_player_pair+1)
-            break
-        else:
-            paytable = [0] * (Baccarat_player_pair+1)
+        paytable = [0] * (Baccarat_player_pair+1)
 
     for winlist in winzone:
         info = winlist.split(odd_split)
         betzone = int(info[0])
         odds = ba_odds[info[1]]
+        if betzone == Baccarat_tie:
+            paytable[Baccarat_banker] = 1
+            paytable[Baccarat_player] = 1
+            
         paytable[betzone] = odds["odds"]
     return paytable
 
--- test/unit/modules/game/broadcast_game/paytable_test.py
+++ test/unit/modules/game/broadcast_game/paytable_test.py
@@ -19,32 +19,32 @@ class DealerTestCase(unittest.TestCase):
 
     def test_baccarat_table(self):
         winstate = ba_paytable.combine_winstate(Baccarat_banker,ba_odds_095)
-        self.assertEqual([0,1.95,0,0,0,0],ba_paytable.paytable(winstate))
+        self.assertEqual([1.95,0,0,0,0],ba_paytable.paytable(winstate))
 
         winstate = ""
         winstate += ba_paytable.combine_winstate(Baccarat_player,ba_odds_1)
-        self.assertEqual([0,0,2,0,0,0],ba_paytable.paytable(winstate))
+        self.assertEqual([0,2,0,0,0],ba_paytable.paytable(winstate))
 
         winstate = ""
         winstate +=  ba_paytable.combine_winstate(Baccarat_tie,ba_odds_8)
-        self.assertEqual([1,1,1,9,1,1],ba_paytable.paytable(winstate))
+        self.assertEqual([1,1,9,0,0],ba_paytable.paytable(winstate))
 
         #multi winstate
         winstate = ""
         winstate = ba_paytable.combine_winstate(Baccarat_banker,ba_odds_095)
         winstate +=  ba_paytable.combine_winstate(Baccarat_banker_pair,ba_odds_11)
-        self.assertEqual([0,1.95,0,0,12,0],ba_paytable.paytable(winstate))
+        self.assertEqual([1.95,0,0,12,0],ba_paytable.paytable(winstate))
 
         winstate = ""
         winstate += ba_paytable.combine_winstate(Baccarat_banker,ba_odds_095)
         winstate += ba_paytable.combine_winstate(Baccarat_banker_pair,ba_odds_11)
         winstate += ba_paytable.combine_winstate(Baccarat_player_pair,ba_odds_11)
-        self.assertEqual([0,1.95,0,0,12,12],ba_paytable.paytable(winstate))
+        self.assertEqual([1.95,0,0,12,12],ba_paytable.paytable(winstate))
 
         winstate = ""
         winstate +=  ba_paytable.combine_winstate(Baccarat_tie,ba_odds_8)
         winstate += ba_paytable.combine_winstate(Baccarat_banker_pair,ba_odds_11)
-        self.assertEqual([1,1,1,9,12,1],ba_paytable.paytable(winstate))
+        self.assertEqual([1,1,9,12,0],ba_paytable.paytable(winstate))
 
 
     #@unittest.skip("skipping")
